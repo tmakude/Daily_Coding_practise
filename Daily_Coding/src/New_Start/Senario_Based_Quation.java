f8d8efd45ff98474ece8026c95e72b94b553e48a
Tushar <tmakude@gmail.com>
2026-02-09 19:19:42 +0530

    What will be the output

---
+++ Daily_Coding/src/New_Start/Senario_Based_Quation.java
@@ -0,0 +1,24 @@
+package New_Start;
+
+public class Senario_Based_Quation {
+
+	public static void main(String[] args) {
+
+
+//		static int test() {
+//			try {
+//				return 3;
+//			}catch (Exception e) {
+//				return 2;
+//			}finally {
+//				return 1;
+//			}
+//		}
+		
+		// What will Answer;
+		
+		//Answer : It will return the 1 ; Cause return value will be overrride
+
+	}
+
+}
